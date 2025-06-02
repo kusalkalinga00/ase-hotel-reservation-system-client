41ce94744513278847261c6a79f26c72fba1156d
Kusal Kalinga <kusalkalingainfo@gmail.com>
2025-06-03 00:48:59 +0530

    shadcn added

---
+++ src/views/HomeView.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomeView = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default HomeView;
